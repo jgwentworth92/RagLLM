01c49a0736c75f3e32bd8d04da1c21a397ce625b
jgwentworth92 <alphabetboi92@protonmail.com>
2024-03-09 16:12:38 -0500

    logging to see to try to fix chat history

--- RagLLM/Router/routes.py
+++ RagLLM/Router/routes.py
@@ -174,7 +174,7 @@ async def create_conversation(conversation: schemas.ConversationCreate,
         raise HTTPException(status_code=500, detail="Internal server error")
 
 
-@router.post("/rag_chain_chat/")
+@router.post("/Rag_chain_chat/")
 async def quick_response(message: schemas.UserMessage, db_session=Depends(db.get_db)):
     Service = LangChainService(model_name=config.SERVICE_MODEL, template=template)
 
