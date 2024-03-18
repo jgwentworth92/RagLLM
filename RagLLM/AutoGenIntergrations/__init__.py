e83f398695906742bf48263af30c52a738d0d0b2
jgwentworth92 <alphabetboi92@protonmail.com>
2024-03-18 19:50:42 -0400

    fixed minor issue with function for agent

--- RagLLM/AutoGenIntergrations/__init__.py
+++ RagLLM/AutoGenIntergrations/__init__.py
@@ -94,7 +94,7 @@ class AutoGenService:
             name="user_proxy",
             human_input_mode="NEVER",
             max_consecutive_auto_reply=10,
-            code_execution_config={"work_dir": "coding"},
+            code_execution_config=False,
             # llm_config_assistant = llm_config_assistant,
             function_map={
                 "answer_PDF_question": self.answer_PDF_question
