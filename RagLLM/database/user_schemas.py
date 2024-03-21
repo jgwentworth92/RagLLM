c441247fd6c7cdf9515fe143980f03bfe24253df
jgwentworth92 <alphabetboi92@protonmail.com>
2024-03-21 12:43:45 -0400

    added pydantic schema

--- RagLLM/database/user_schemas.py
+++ RagLLM/database/user_schemas.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel
 
 class UserBase(BaseModel):
@@ -11,3 +13,6 @@ class UserCreate(UserBase):
     Users creation schema
     """
     pass
+
+class Sentences(BaseModel):
+    sentences: List[str]
